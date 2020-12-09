0500b671d0a1568b85ac41b2a1d67a5cdd4ced01
syakoo <nichijou4009@gmail.com>
2020-12-09 19:56:42 +0900

    initial commit

---
+++ setup.py
@@ -0,0 +1,14 @@
+import setuptools
+
+
+setuptools.setup(
+    name="Hello World",
+    version="0.0",
+    author="syakoo",
+    author_email="hogehoge@hoge.hoge",
+    description="Say Hello.",
+    url="https://github.com/syakoo/advent-calender2020_python2",
+    packages=["hello_world"],
+    package_dir={"hello_world": "src/hello_world"},
+    python_requires=">=3.8"
+)
---
+++ src/hello_world/say_hello.py
@@ -0,0 +1,10 @@
+def hello(your_name: str) -> str:
+    """挨拶を返す関数
+
+    Args:
+        your_name (str): あなたの名前
+
+    Returns:
+        str: 挨拶
+    """
+    return f'Hello, {your_name}.'
